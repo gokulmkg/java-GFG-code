763a8b6f1322c3d97c3a2a0509c92ccf6f232637
GOKUL.M <144157488+gokulmkg@users.noreply.github.com>
2025-03-17 11:33:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.java
+++ Difficulty: Basic/Array Search/array-search.java
@@ -32,12 +32,10 @@ System.out.println("~");
 class Solution {
 
     static int search(int arr[], int x) {
-
-        // Your code here
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==x)
-            return i;
-        }
-        return -1;
+     for(int i=0;i<arr.length;i++){
+         if(arr[i]==x)
+         return i;
+     }
+     return -1;
     }
 }
