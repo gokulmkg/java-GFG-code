0a23edafe89bed55b748f69ccec5c06f2363cd76
GOKUL.M <144157488+gokulmkg@users.noreply.github.com>
2025-04-11 12:22:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -29,6 +29,8 @@ class Main {
 
             // calling maxSubarraySum() function
             System.out.println(obj.maxSubarraySum(arr));
+
+            System.out.println("~");
         }
     }
 }
@@ -36,22 +38,19 @@ class Main {
 // } Driver Code Ends
 
 
-// User function Template for Java
 class Solution {
-
-    // arr: input array
-    // Function to find the sum of contiguous subarray with maximum sum.
     int maxSubarraySum(int[] arr) {
-         if(arr==null)
-         return 0;
-         
-         int currentmax=arr[0];
-         int totalmax=arr[0];
-         
-         for(int i=1;i<arr.length;i++){
-             currentmax=Math.max(arr[i],currentmax+arr[i]);
-             totalmax=Math.max(currentmax,totalmax);
-         }
-         return totalmax;
+     int max=Integer.MIN_VALUE;
+     int sum=0;
+   
+     for(int i=0;i<arr.length;i++){
+        sum+=arr[i];
+        if(sum>max)
+        max=sum;
+        
+        if(sum<0)
+        sum=0;
+     }
+     return max;
     }
 }
