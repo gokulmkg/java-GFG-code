7b559353900bb2935a375a4ba16ad011a1a0625e
GOKUL.M <144157488+gokulmkg@users.noreply.github.com>
2025-06-01 19:59:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -1,56 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution obj = new Solution();
-
-            // calling maxSubarraySum() function
-            System.out.println(obj.maxSubarraySum(arr));
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     int maxSubarraySum(int[] arr) {
-     int max=Integer.MIN_VALUE;
-     int sum=0;
-   
-     for(int i=0;i<arr.length;i++){
-        sum+=arr[i];
-        if(sum>max)
-        max=sum;
-        
-        if(sum<0)
-        sum=0;
-     }
-     return max;
+        // Your code here
+        int sum =0;
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<arr.length;i++) {
+            sum+= arr[i];
+            if(sum > max) {
+                max = sum;
+            }
+            if(sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
     }
 }
